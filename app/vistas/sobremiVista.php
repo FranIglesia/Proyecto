395ad49c566b32a39f4d7898179c687f35f3a950
FranIglesia <franciscojoseiglesia@gmail.com>
2022-07-30 17:34:33 +0200

    creado vistas sobre mi y contacto

---
+++ app/vistas/sobremiVista.php
@@ -0,0 +1,21 @@
+<?php include_once("encabezado.php"); 
+print "<h2 class='text-center'>Sobre mi</h2>"; 
+print "<div class='alert mt-3'>";
+print "<img src='img/fran.png' class='rounded float-right'/>";
+print "<p>Francisco José Iglesia Martín, persona dedicada a la implantación de novedades y actualizaciones de cursos de informática aplicada como a la de publicación independiente y de marketing que libera al genio creativo dentro de cada uno. La plataforma  facilita la compra a de libros impresos y electrónicos de calidad profesional, asi como poder aprender diferentes lenguajes de informática con los cursos impartidos por lo diferentes profesores.
+
+La empresa, Fundada por Francisco Iglesia en 2020,  incluye un equipo de expertos en diseño, Internet y medios que comparten la pasión de ayudar a las personas a hacer realidad sus historias. </p>";
+print "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate molestiae saepe rem sunt quaerat sapiente quibusdam vel, blanditiis quam quas beatae atque excepturi corporis accusantium, unde illum architecto delectus sint?</p>";
+print "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam officiis doloremque tempore, quae non voluptas repellendus minima deserunt odio quo cum, accusantium consequatur aliquid excepturi vitae cupiditate amet sapiente quis.</p>";
+print "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis illo, sequi nostrum omnis exercitationem dolorem sint. Voluptatum delectus, doloremque dignissimos eveniet rem consequuntur at porro amet, libero ipsa quos autem!</p>";
+print "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate molestiae saepe rem sunt quaerat sapiente quibusdam vel, blanditiis quam quas beatae atque excepturi corporis accusantium, unde illum architecto delectus sint?</p>";
+print "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam officiis doloremque tempore, quae non voluptas repellendus minima deserunt odio quo cum, accusantium consequatur aliquid excepturi vitae cupiditate amet sapiente quis.</p>";
+print "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam officiis doloremque tempore, quae non voluptas repellendus minima deserunt odio quo cum, accusantium consequatur aliquid excepturi vitae cupiditate amet sapiente quis.</p>";
+print "<br>";
+print "<br>";
+print "<br>";
+print "</div>";
+print "<a href='".RUTA."tienda' class='btn btn-success'/>Regresar</a>";
+
+
+include_once("piepagina.php"); ?>
