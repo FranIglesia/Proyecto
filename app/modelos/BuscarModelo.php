ee382ecc46f8feaa7cbb124c4c80e9a78a26edcc
FranIglesia <franciscojoseiglesia@gmail.com>
2022-08-01 07:19:21 +0200

    despliegue busca productos

--- app/modelos/BuscarModelo.php
+++ app/modelos/BuscarModelo.php
@@ -9,5 +9,16 @@ class BuscarModelo{
   {
     $this->db = new MySQLdb();
   }
+
+  function getProductosBuscar($buscar){
+    $sql = "SELECT * FROM productos WHERE ";
+    $sql.= "autor LIKE '%".$buscar."%' OR ";
+    $sql.= "editorial LIKE '%".$buscar."%' OR ";
+    $sql.= "nombre LIKE '%".$buscar."%' OR ";
+    $sql.= "descripcion LIKE '%".$buscar."%'";
+    //
+    $data = $this->db->querySelect($sql);
+    return $data; 
+  }
 }
 ?>
--- app/vistas/buscarVista.php
+++ app/vistas/buscarVista.php
@@ -5,10 +5,10 @@
             $ren = 0;
             for ($i=0; $i < count($datos["data"]); $i++) { 
               if ($ren==0) {
-                print "<div class='row'>";
+                print "<div class='row'>"; 
               }
               print "<div class='card pt-2 col-sm-3'>";
-              print "<img src='img/".$datos['data'][$i]["imagen"]."' ";
+              print "<img src='".RUTA."img/".$datos['data'][$i]["imagen"]."' ";
               print "class='img-responsive' style='width:100%; height:140px;' ";
               print "alt='".$datos['data'][$i]["nombre"]."'/>";
               print "<p><a href='".RUTA."admonProductos/producto/";
@@ -21,28 +21,8 @@
                 print "</div>";
               }
             }
-            print "<br>";
-            print '<h1 class="text-center">Artículos nuevos</h1>';
-            print '<div class="card p-4 bg-light">';
-            $ren = 0;
-            for ($i=0; $i < count($datos["nuevos"]); $i++) { 
-              if ($ren==0) {
-                print "<div class='row'>";
-              }
-              print "<div class='card pt-2 col-sm-3'>";
-              print "<img src='img/".$datos['nuevos'][$i]["imagen"]."' ";
-              print "class='img-responsive' style='width:100%; height:140px;' ";
-              print "alt='".$datos['nuevos'][$i]["nombre"]."'/>";
-              print "<p><a href='".RUTA."admonProductos/producto/";
-              print $datos['nuevos'][$i]["id"]."'>";
-              print $datos['nuevos'][$i]["nombre"]."</a></p>";
-              print "</div>";
-              $ren++;
-              if ($ren==4) {
-                $ren = 0;
-                print "</div>";
-              }
-            }
           ?>
-        </div><!--card--> 
+        </div><!--card-->
+        <br><br><br><br><br><br><br><br><br><br>
+        <br><br><br><br><br><br><br><br><br>
 <?php include_once("piepagina.php"); ?>
